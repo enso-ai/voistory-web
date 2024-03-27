79ccfe2351b4c298dc921a9c3f915acc93a3eae2
yanzhi <yanzhi@enso-ai.com>
2024-03-27 06:14:30 -0700

    add utils/audio to provide accessibility on aduio input/output devices

---
+++ src/utils/audio.js
@@ -0,0 +1,166 @@
+export class Speaker {
+    constructor() {
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        this.frames = [];
+        this.speakerOn = false;
+    }
+
+    // decode base64 string to float32Array that can be consumed by the audio context
+    decodeAudioData(base64String) {
+        // base64 decoding to binary string
+        const binaryString = atob(base64String);
+
+        // binary string to bytes
+        const bytes = new Uint8Array(binaryString.length);
+        for (let i = 0; i < binaryString.length; i++) {
+            bytes[i] = binaryString.charCodeAt(i);
+        }
+
+        // bytes to float32 with normalization
+        const int16Array = new Int16Array(bytes.buffer);
+        const float32Array = new Float32Array(int16Array.length);
+        for (let i = 0; i < int16Array.length; i++) {
+            float32Array[i] = int16Array[i] / 32768.0;
+        }
+
+        // final output is float32Array with values between -1 and 1
+        return float32Array;
+    }
+
+    pushAudioData(base64String) {
+        this.frames.push(this.decodeAudioData(base64String));
+    }
+
+    async run() {
+        this.speakerOn = true;
+        let nextBufferStartTime = 0;
+        while (this.speakerOn) {
+            // frame is not empty, draw audio data from it
+            if (this.frames.length > 1) {
+                // pop first chunk
+                const audioData = this.frames.shift();
+
+                // create source buffer for audio data consumption, copy audioData to the buffer
+                const source = this.audioContext.createBufferSource();
+                source.buffer = this.audioContext.createBuffer(1, audioData.length, 16000);
+                source.buffer.copyToChannel(audioData, 0, 0);
+                source.connect(this.audioContext.destination);
+
+                // if the last updated next buffer start time is in the past
+                // update it to the current time
+                if (nextBufferStartTime < this.audioContext.currentTime) {
+                    nextBufferStartTime = this.audioContext.currentTime;
+                }
+
+                // set the next projected buffer start time
+                nextBufferStartTime += source.buffer.duration;
+
+                // schedule the chunk to be played at the next buffer start time
+                // notice this function is not blocking. ie it will return immediately
+                // even tho the audio is still playing. And multiple sources can be played
+                // in parallel. Therefore, we need to set the next buffer start time to make
+                // sure they are played in order.
+                source.start(nextBufferStartTime);
+            } else {
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
+        }
+    }
+
+    stop() {
+        this.speakerOn = false;
+        this.frames = [];
+    }
+}
+
+export class Microphone {
+    constructor() {
+        this.audioStream = null;
+        this.streaming = false;
+    }
+
+    async run(handleData) {
+        this.audioCtx = new AudioContext({ sampleRate: 16000 });
+        await this.audioCtx.audioWorklet.addModule('input-processor.js');
+        this.stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            channelCount: 1,
+        });
+        this.sourceNode = this.audioCtx.createMediaStreamSource(this.stream);
+        this.processorNode = new AudioWorkletNode(this.audioCtx, 'recording-processor');
+
+        this.frames = [];
+        this.groupChunks(handleData);
+        // Set up message handling from the AudioWorklet
+        this.processorNode.port.onmessage = (event) => {
+            this.frames.push(event.data);
+        };
+
+        this.sourceNode.connect(this.processorNode).connect(this.audioCtx.destination);
+    }
+
+    async groupChunks(handleData) {
+        this.streaming = true;
+        while (this.streaming) {
+            if (this.frames.length >= 10) {
+                const chunkFrames = this.frames.splice(0, 10);
+                const chunkSize = chunkFrames[0].length
+                let pcmData = new Int16Array(chunkSize * 10);
+                for (let i = 0; i < chunkFrames.length; i++) {
+                    const frame = chunkFrames[i];
+                    if (pcmData === null) {
+                        pcmData.set(frame);
+                    } else {
+                        pcmData.set(frame, i * chunkSize);
+                    }
+                }
+                // Here, you could send the Base64-encoded data via WebSocket or handle it as needed
+                handleData(this.int16ToBase64(pcmData));
+            } else {
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
+        }
+    }
+
+    int16ToBase64(int16Array) {
+        // Create a DataView directly from the Int16Array
+        const view = new DataView(int16Array.buffer);
+        
+        // Then, encode the DataView to Base64
+        let binary = '';
+        for (let i = 0; i < int16Array.length * 2; i++) {
+            binary += String.fromCharCode(view.getUint8(i));
+        }
+
+        return window.btoa(binary) 
+    }
+
+    stop() {
+        // Stop all tracks on the stream to release the media device
+        this.streaming = false;
+
+        const stream = this.stream
+        const sourceNode = this.sourceNode
+        const processorNode = this.processorNode
+        const audioContext = this.audioCtx
+        if (stream && stream.getTracks) {
+            stream.getTracks().forEach(track => track.stop());
+        }
+
+        // Disconnect the sourceNode and processorNode from the AudioContext
+        if (sourceNode) {
+            sourceNode.disconnect();
+        }
+        if (processorNode) {
+            processorNode.disconnect();
+        }
+
+        // Close the AudioContext to release audio processing resources
+        if (audioContext) {
+            audioContext.close().then(() => {
+                console.log('AudioContext closed and resources released');
+            });
+        }
+    }
+}
+
